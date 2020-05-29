655ba3d318e5f266b14a7ba7388c34a44a45bffe
Jan Henning <triton539@hotmail.com>
2020-05-29 04:06:40 +0200

    maps & agent individual pages

---
+++ src/pages/agents/[agent].js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout'
+
+const AgentPage = () => {
+  return (
+    <Layout>
+      <h1>Individual Agent Page</h1>
+    </Layout>
+  )
+}
+
+export default AgentPage
---
+++ src/pages/maps/[map].js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout'
+
+const MapPage = () => {
+  return (
+    <Layout>
+      <h1>Individual Map Page</h1>
+    </Layout>
+  )
+}
+
+export default MapPage

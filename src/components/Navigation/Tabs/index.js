eaa550e41ef1838554c46fe3097104e37bbf28cf
Jan Henning <triton539@hotmail.com>
2020-06-21 09:47:18 +0200

    weapons specific page

--- src/components/Navigation/Tabs/index.js
+++ src/components/Navigation/Tabs/index.js
@@ -2,10 +2,13 @@ import CustomLink from '../CustomLink'
 
 import { maps } from '../../../data/maps.json'
 import { agents } from '../../../data/agents.json'
+import { sidearms, smgs, shotguns, rifles, snipers, heavies } from '../../../data/weapons.json'
+
+const allWeapons = [ ...sidearms, ...smgs, ...shotguns, ...rifles, ...snipers, ...heavies ]
 
 export const MapTabs = () => {
   return (
-    <nav className='tabs-nav-wrapper'>
+    <nav className='tabs-nav-wrapper maps'>
       <div className='tabs-nav'>
         {maps.map((el, i) => (
           <CustomLink key={i} href='/maps/[map]' as={el.path} activeClassName='tabs-nav__link-active'>
@@ -19,10 +22,24 @@ export const MapTabs = () => {
 
 export const AgentTabs = () => {
   return (
-    <nav className='tabs-nav-wrapper'>
+    <nav className='tabs-nav-wrapper agents'>
       <div className='tabs-nav'>
         {agents.map((el, i) => (
           <CustomLink key={i} href='/agents/[agent]' as={el.path} activeClassName='tabs-nav__link-active'>
+            <a className='tabs-nav__link'>{el.name}</a>
+          </CustomLink>
+        ))}
+      </div>
+    </nav>
+  )
+}
+
+export const WeaponTabs = () => {
+  return (
+    <nav className='tabs-nav-wrapper weapons'>
+      <div className='tabs-nav'>
+        {allWeapons.map((el, i) => (
+          <CustomLink key={i} href='/weapons/[weapon]' as={el.path} activeClassName='tabs-nav__link-active'>
             <a className='tabs-nav__link'>{el.name}</a>
           </CustomLink>
         ))}
--- src/pages/weapons/[weapon].js
+++ src/pages/weapons/[weapon].js
@@ -1,11 +1,61 @@
 import Layout from '@/components/Layout'
+import { WeaponTabs } from '@/components/Navigation/Tabs'
 import { getWeapon, getWeaponPaths } from '@/utils/API'
+import { CDN } from '@/utils/helpers'
 
 const WeaponPage = ({ weapon }) => {
   return (
     <Layout>
-      <h1 className='mt-2'>Individual Weapon Page</h1>
-      <h1>{weapon.name}</h1>
+      <WeaponTabs />
+      <div className='weapon-page-wrapper'>
+        <div className='weapons-weapon'>{/* <img src={CDN(map.images.map.layout)} alt={`VALORANT Map ${weapon.name}`} /> */}</div>
+        <div className='weapon-info'>
+          <div className='weapons-group'>
+            <h4 className='text-center text-lg'>Weapon Overview</h4>
+            <div className='relative'>
+              <span className={`weapon__tier weapon__tier-${weapon.tier} top-0 left-0`}>{weapon.tier} tier</span>
+              <picture>
+                <img
+                  src={CDN(weapon.images.model)}
+                  className='weapon-hero'
+                  alt={`VALORANT Weapon ${weapon.name}`}
+                  title={`VALORANT Weapon ${weapon.name}`}
+                />
+              </picture>
+              <h4 className='weapon-name'>{weapon.name}</h4>
+              <dl className='weapon__desc-list weapon__desc-list_main'>
+                <dt className='weapon__desc-title'>Sidearm</dt>
+                <dd className='weapon__desc-text'>
+                  <img className='price_credits' src={CDN('/images/price_credits.svg')} alt='Weapon Cost' title='Weapon Cost' />
+                  {weapon.cost}
+                </dd>
+              </dl>
+              <dl className='weapon__desc-list weapon__desc-list_secondary'>
+                <dt className='weapon__desc-title'>Magazine Capacity</dt>
+                <dd className='weapon__desc-text'>{weapon.magazine}</dd>
+                <dt className='weapon__desc-title'>Wall Penetration</dt>
+                <dd className='weapon__desc-text'>{weapon.penetration}</dd>
+                <h3 className='weapon__desc-title section-title'>Primary Fire</h3>
+                <dd></dd>
+                <dt className='weapon__desc-title'>Mode</dt>
+                <dd className='weapon__desc-text'>{weapon.fire.primary.mode}</dd>
+                <dt className='weapon__desc-title'>Fire Rate</dt>
+                <dd className='weapon__desc-text'>{weapon.fire.primary.rate}</dd>
+                {weapon.fire.alt && (
+                  <>
+                    <h3 className='weapon__desc-title section-title'>Alternate Fire</h3>
+                    <dd></dd>
+                    <dt className='weapon__desc-title'>Mode</dt>
+                    <dd className='weapon__desc-text'>{weapon.fire.alt.mode}</dd>
+                    <dt className='weapon__desc-title'>Fire Rate</dt>
+                    <dd className='weapon__desc-text'>{weapon.fire.alt.rate}</dd>
+                  </>
+                )}
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
